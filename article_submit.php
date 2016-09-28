5772449d39c8da9dc0e021d9963879f0dec2a2e1
Colin Keating <ckeating3000@gmail.com>
2016-09-28 14:15:16 -0500

    article submit and link submit

--- article_submit.php
+++ article_submit.php
@@ -4,28 +4,26 @@
 	//open mod3 database
 	require database.php
 
-	$article = $_POST['article_submit'];
-	
+	//list of variables to add to table
+	$article=$_POST['article_submit'];
+	$title=$_POST['article_title'];
+	$username=$_SESSION['login'];
 
 	//check to make sure article text contains no funny characters
 	if( !preg_match('/^[\w_\-]+$/', $article) ){
 		echo "Invalid article text; remove special characters";
 		exit;
 	}
-
 	
 	//if article text is valid, add it to mysql mod3 database
- 	if(link_exists){
-		$stmt = $mysqli->prepare("insert into mod3 (story_text) values (?)");
-		if(!$stmt){
-			printf("Query Prep Failed: %s\n", $mysqli->error);
-			exit;
-		}
-
-		$stmt->bind_param('s', $article);
-		 
-		$stmt->execute();
-		 
-		$stmt->close();
+	$stmt = $mysqli->prepare("insert into mod3 (story_text, username, story_title) values (?, ?, ?)");
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
 	}
+
+	$stmt->bind_param('sss', $article, $username, $story_title);
+	$stmt->execute();
+	$stmt->close();
+	
 ?>
