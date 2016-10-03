f2586822bbd9ba1f1dd4338f850977e68a0d9e08
Kate Harline <kharline@wustl.edu>
2016-10-02 23:11:42 -0500

    debug

--- delete_comments_posts.php
+++ delete_comments_posts.php
@@ -23,44 +23,12 @@
 			    header("Location: home_page_nologin.php");
 			    exit;
 		    }
-
-		    echo "<br>\n<br>\n";
-		   echo "Your posts";
-        //DISPLAY/DELETE POSTS
-            $username = $_SESSION['Login'];
+$username = $_SESSION['Login'];
             require 'database_rw.php';
-            
-            $get_posts = $mysqli->prepare("select link, story_text, username, story_title, story_id, comment_count from stories where username=? order by story_id");
-            if(!$get_posts){
-                printf("Query Prep Failed: %s\n", $mysqli->error);
-                exit;
-            }
-			$get_posts->bind_param('s', $username);
-            $get_posts->execute();
-            $get_posts->bind_result($link, $text, $username, $title, $id, $comcount);
-            
-            echo "<ul>\n";
-			
-            while($get_posts->fetch()){
-                printf("\t<li> <a href='%s'>%s</a> <br> %s <br> <br>
-					   <a href='delete_post.php?name=%s'>delete this post</a>
-					   </li><br>\n",
-                    htmlspecialchars($link),
-                    htmlspecialchars($title),
-                    htmlspecialchars($text),
-					htmlentities($id)
-                );
-
-				//allow the person to delete posts that are their own
-				//if($username==$_SESSION["Login"]){
-				//	echo "<a href='delete.php?name=$title'>Delete this Post</a>";
-				//}
-            }
-            echo "</ul>\n";
-            $get_posts->close();
-
+		    echo "<br>\n<br>\n";
+		   
         //DISPLAY/DELETE COMMENTS
-echo "Your comments";
+	echo "Your comments";
             $get_comments = $mysqli->prepare("select comment, story_id, username, comment_id from comments where username=? order by story_id");
             if(!$get_comments){
                 printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -84,7 +52,34 @@ echo "Your comments";
             echo "</ul>\n";
             $get_comments->close();
             
-            //destroy the session after someone hits the logout button, send them back to the login screen
+            echo "Your posts";
+			
+        //DISPLAY/DELETE POSTS
+            
+            $get_posts = $mysqli->prepare("select link, story_text, username, story_title, story_id, comment_count from stories where username=? order by story_id");
+            if(!$get_posts){
+                printf("Query Prep Failed: %s\n", $mysqli->error);
+                exit;
+            }
+			$get_posts->bind_param('s', $username);
+            $get_posts->execute();
+            $get_posts->bind_result($link, $text, $username, $title, $id, $comcount);
+            
+            echo "<ul>\n";
+			
+            while($get_posts->fetch()){
+                printf("\t<li> <a href='%s'>%s</a> <br> %s <br> <br>
+					   <a href='delete_post.php?name=%s'>delete this post</a>
+					   </li><br>\n",
+                    htmlspecialchars($link),
+                    htmlspecialchars($title),
+                    htmlspecialchars($text),
+					htmlentities($id)
+                );
+            }
+            echo "</ul>\n";
+            $get_posts->close();
+
             
             ?>
         </body>
