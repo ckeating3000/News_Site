8c056d06a3db5ef8ae33cbdb2847064a79f7b100
Colin Keating <ckeating3000@gmail.com>
2017-01-26 20:29:09 -0600

    Delete database_r.php

--- database_r.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connect to database with only ability to select from mod3 tables
-$mysqli = new mysqli('localhost', 'read', 'read', 'mod3');
-if($mysqli->connect_errno) {
-	printf("Connection Failed: %s\n", $mysqli->connect_error);
-	exit;
-}
-?>
