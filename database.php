6d52a9da9ca0d1dcb7a97dc170c77171e2ee2c04
Colin Keating <ckeating3000@gmail.com>
2017-01-26 20:28:19 -0600

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-//connect to mod3 database with all permissions
-$mysqli = new mysqli('localhost', 'wustl_inst', 'wustl_pass', 'mod3');
-if($mysqli->connect_errno) {
-	printf("Connection Failed: %s\n", $mysqli->connect_error);
-	exit;
-}
-?>
