c6afc13b454d733bd9a5125eea512d317aeeea08
Colin Keating <ckeating3000@gmail.com>
2017-01-26 20:19:04 -0600

    First commit

--- like_post.php
+++ like_post.php
@@ -4,20 +4,25 @@ session_start();
 $liker = $_SESSION["Login"];
 $story_id = $_GET["name"];
 
-//increase the value of likes by one
-$like=$mysqli->prepare("update stories set num_likes=num_likes +1 where story_id=?");
-$like->bind_param('i', $story_id);
-$like->execute();
-$like->close();
-//update liker string
+// check if user has already liked the post
 $liker_string = " ".$liker."\r\n";
-//add new user to the list of likers
-//adapted from http://stackoverflow.com/questions/19081743/how-can-i-add-text-to-sql-column
-$likers=$mysqli->prepare("update stories set likers= CONCAT('$hliker_string', likers) where story_id=?");
-$likers->bind_param('i', $story_id);
-$likers->execute();
-$likers->bind_result($like_list);
-$likers->close();
+if(strpos($liker_string, $liker)===false){
+	//increase the value of likes by one
+	$like=$mysqli->prepare("update stories set num_likes=num_likes +1 where story_id=?");
+	$like->bind_param('i', $story_id);
+	$like->execute();
+	$like->close();
+	//update liker string
+	$liker_string = " ".$liker."\r\n";
+	//add new user to the list of likers
+	//adapted from http://stackoverflow.com/questions/19081743/how-can-i-add-text-to-sql-column
+	$likers=$mysqli->prepare("update stories set likers= CONCAT('$hliker_string', likers) where story_id=?");
+	$likers->bind_param('i', $story_id);
+	$likers->execute();
+	$likers->bind_result($like_list);
+	$likers->close();
+}
+
 
 
 
--- view_likes.php
+++ view_likes.php
@@ -17,57 +17,57 @@
 				
             </nav>
         
-               <?php
-			   session_start();
-				 if(!isset($_SESSION['Login'])){
-				Header("Location: home_page_nologin.php");
-				exit;
-			   }
-                $story_id = $_GET["name"];
-				$_SESSION["story_id"]=$story_id;
-				
-               require 'database_r.php';
-			   echo "<br> <br> <br>";
-               //display the article
-                $get_story = $mysqli->prepare("select link, story_text, username, story_title from stories where story_id=?");
-                if(!$get_story){
-                    printf("Query Prep Failed: %s\n", $mysqli->error);
-                    exit;
-                }
-				
-                $get_story->bind_param('i', $story_id);
-                $get_story->execute();
-                $get_story->bind_result($link, $text, $username, $title);
-				$get_story->fetch();
-                printf("\t <a href='%s'>%s</a> <br> %s <br> %s <br>
-						   <br>\n",
-                        htmlspecialchars($link),
-                        htmlspecialchars($title),
-                        htmlspecialchars($text), 
-                        "Posted by: ".htmlspecialchars($username)
-                      );
-                $get_story->close();
-                 
-                //display a list of who liked the post...string of text from the stories database
-                
-                $get_likes = $mysqli->prepare("select likers from stories where story_id=?");
-                if(!$get_likes){
-                    printf("Query Prep Failed: %s\n", $mysqli->error);
-                    exit;
-                }
-                $get_likes->bind_param('i', $story_id);
-                $get_likes->execute();
-                 
-                $get_likes->bind_result($likers);
-                echo "Users who have liked this post: ";
-                while($get_likes->fetch()){
-                    printf("\t %s  
-						   \n",
-                        htmlspecialchars($likers)
-                    );
-                }
-                 
-                $get_likes->close();
+                <?php
+    			    session_start();
+    				 if(!isset($_SESSION['Login'])){
+    				Header("Location: home_page_nologin.php");
+    				exit;
+    			    }
+                    $story_id = $_GET["name"];
+    				$_SESSION["story_id"]=$story_id;
+    				
+                    require 'database_r.php';
+    			    echo "<br> <br> <br>";
+                    //display the article
+                    $get_story = $mysqli->prepare("select link, story_text, username, story_title from stories where story_id=?");
+                    if(!$get_story){
+                        printf("Query Prep Failed: %s\n", $mysqli->error);
+                        exit;
+                    }
+    				
+                    $get_story->bind_param('i', $story_id);
+                    $get_story->execute();
+                    $get_story->bind_result($link, $text, $username, $title);
+    				$get_story->fetch();
+                    printf("\t <a href='%s'>%s</a> <br> %s <br> %s <br>
+    						   <br>\n",
+                            htmlspecialchars($link),
+                            htmlspecialchars($title),
+                            htmlspecialchars($text), 
+                            "Posted by: ".htmlspecialchars($username)
+                          );
+                    $get_story->close();
+                     
+                    //display a list of who liked the post...string of text from the stories database
+                    
+                    $get_likes = $mysqli->prepare("select likers from stories where story_id=?");
+                    if(!$get_likes){
+                        printf("Query Prep Failed: %s\n", $mysqli->error);
+                        exit;
+                    }
+                    $get_likes->bind_param('i', $story_id);
+                    $get_likes->execute();
+                     
+                    $get_likes->bind_result($likers);
+                    echo "Users who have liked this post: ";
+                    while($get_likes->fetch()){
+                        printf("\t %s  
+    						   \n",
+                            htmlspecialchars($likers)
+                        );
+                    }
+                     
+                    $get_likes->close();
                 
                 ?>
         </body>
