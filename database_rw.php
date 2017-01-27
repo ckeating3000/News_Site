9de3bddbc544b72be62036323416609ddbe1bb49
Colin Keating <ckeating3000@gmail.com>
2017-01-26 20:28:55 -0600

    Delete database_rw.php

--- database_rw.php
+++
@@ -1,8 +0,0 @@
-<?php
-//connect to mod3 database with ability to update, select and insert into tables
-$mysqli = new mysqli('localhost', 'readwrite', 'readwrite', 'mod3');
-if($mysqli->connect_errno) {
-	printf("Connection Failed: %s\n", $mysqli->connect_error);
-	exit;
-}
-?>
